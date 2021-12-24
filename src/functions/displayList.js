d9604e18f75557c8d9bb94fe7cb0a69eb5c512ae
mohamedelshamy55 <mohamedelshamy55@gmail.com>
2021-12-24 09:57:45 +0200

    Add new like

--- src/functions/displayList.js
+++ src/functions/displayList.js
@@ -1,25 +1,54 @@
 import { getMealsList } from './mealsApi.js';
+import { getLikes } from './mealsApi.js';
+
 
 const mealsList = document.querySelector('#main-page');
 
-export default async () => {
-  const data = await getMealsList();
-  let htmlCode = '';
-  data.meals.forEach((item) => {
-    htmlCode += `
+const displayList = async() => {
+    const data = await getMealsList();
+    const likes = await getLikes();
+    console.log(likes)
+    let htmlCode = '';
+    data.meals.forEach((item, index) => {
+        console.log(item.idMeal + '  idMeal')
+        console.log(likes.filter((el) => {
+            el.item_id === item.idMeal
+        }))
+        htmlCode += `
         <div id="card">
             <img src="${item.strMealThumb}" alt="">
             <div id='description'>
                 <h3>${item.strMeal}</h3>
-                <p><i class="fas fa-heart"></i> 0 Likes</p>
+                <p class='likeNumber' data-id=${item.idMeal}><i class="fas fa-heart"></i> ${likes[index].likes} Likes</p>
             </div>
             <div id='btns'>
-                <button id=${item.idMeal} class='comment-btn'><i class="fas fa-comment"></i> Comments</button>
+            
+                <button id='comment-btn'><i class="fas fa-comment"></i> Comments</button>
+                <button id='reservation-btn'><i class="fas fa-ticket-alt"></i> Reservations</button>
             </div>
         </div>
       `;
-  });
-  mealsList.innerHTML = htmlCode;
+    });
+    mealsList.innerHTML = htmlCode;
 
-  return document.querySelectorAll('.comment-btn');
+    const likeButton = document.querySelectorAll('.fa-heart');
+    const likeNumber = document.querySelectorAll('.likeNumber');
+   likeButton.forEach((element, index) => {
+    element.addEventListener('click', async () => {
+      await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/bjeHLEObVtTi6m0Zp622/likes', {
+        method: 'POST',
+        body: JSON.stringify({
+          item_id: `item${index}`,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      likes = await getLikes();
+      likeNumber[index].innerHTML = `${likes[index].likes} Likes`
+
+    });
+  });
 };
+
+export default displayList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+/* eslint-disable */
 import './style.css';
 import displayList from './functions/displayList.js';
-import comment from './functions/comment.js';
+import addLike from './functions/likes.js';
 
-window.addEventListener('load', async () => {
-  const commentBtn = await displayList();
 
-  commentBtn.forEach(async (btn) => {
-    btn.addEventListener('click', () => {
-      comment(btn);
-    });
-  });
-});
+
+document.addEventListener('click', async(e) => {
+    if (e.target.matches('.fas.fa-heart')) {
+
+        const item_id = Number(e.target.idMeal)
+        let like = Number(e.target.nextSibling.textContent.match(/[0-9]/g).join(''));
+        e.target.nextSibling.textContent = `${like + 1} likes`;
+        //await addLike(item_id);
+    }
+})
+displayList();
