c413bb806c1ac431d24fc0f8a0e230b329650c78
Khalil <khalilnasri92@gmail.com>
2021-12-25 00:56:26 +0100

    required changes

--- src/functions/displayList.js
+++ src/functions/displayList.js
@@ -90,9 +90,9 @@ const mealPopUp = async (meal) => {
     </div>
     <h3 class="m-3">Add a comment</h3>
     <form autocomplete="off" class="form-class">
-      <input type="text" class="form-control" id="commentator" required placeholder="Your name" required>
-      <textarea id="comment" name="comment" cols="30" rows="10" required placeholder="Your comment..." required></textarea>
-      <button type="button" class="btn commentBtn">Comment</button>
+      <input type="text" class="form-control" id="commentator" required placeholder="Your name">
+      <textarea id="comment" name="comment" cols="30" rows="10" required placeholder="Your comment..."></textarea>
+      <button type="submit" class="btn commentBtn">Comment</button>
     </form>
 
   `;
